f296a4dbfe63af50093f3c4593adfaeb40cd0969
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-01-24 23:57:58 +0700

    create pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
