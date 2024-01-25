ce758bb4f208566c8ad0f7a47b6bba7a5399a390
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-01-25 15:01:58 +0700

    complete header ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import About from './pages/About'
 import Dashboard from './pages/Dashboard'
 import Home from './pages/Home'
@@ -6,9 +7,12 @@ import Projects from './pages/Projects'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 
+import Header from './components/Header'
+
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
