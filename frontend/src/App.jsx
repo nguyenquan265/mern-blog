8f6c071733e72b51d4051bebd3c9facad5799bc9
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-01-25 20:21:46 +0700

    complete footer ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/projects' element={<Projects />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
