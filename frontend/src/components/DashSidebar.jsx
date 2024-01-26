1471e5d30643ffc9d4a001e193f4a6fa49459611
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-01-26 19:25:58 +0700

    complete basic sidebar

---
+++ frontend/src/components/DashSidebar.jsx
@@ -0,0 +1,39 @@
+import { Sidebar } from 'flowbite-react'
+import { HiUser, HiArrowSmRight } from 'react-icons/hi'
+import { useState, useEffect } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+
+function DashSideBar() {
+  const location = useLocation()
+  const [tab, setTab] = useState('')
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get('tab')
+
+    if (tabFromUrl) {
+      setTab(tabFromUrl)
+    }
+  }, [location])
+
+  return (
+    <Sidebar className='w-full md:w-56'>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to='/dashboard?tab=profile'>
+            <Sidebar.Item
+              active={tab === 'profile'}
+              icon={HiUser} label='User'
+              labelColor='dark'>
+              Profile
+            </Sidebar.Item></Link>
+          <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  )
+}
+
+export default DashSideBar
