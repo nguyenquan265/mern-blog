02569e006db3d93bdcbf66619039a876f535f2a0
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-01-29 14:52:39 +0700

    complete get posts api

--- backend/src/apis/controllers/post.controller.js
+++ backend/src/apis/controllers/post.controller.js
@@ -22,3 +22,45 @@ export const create = catchAsync(async (req, res, next) => {
 
   res.status(201).json({ message: 'Create post successfully', post })
 })
+
+export const getPosts = catchAsync(async (req, res, next) => {
+  const startIndex = parseInt(req.query.startIndex) || 0
+  const limit = parseInt(req.query.limit) || 9
+  const sortDirection = req.query.order === 'asc' ? 1 : -1
+
+  const posts = await Post.find({
+    ...(req.query.userId && { userId: req.query.userId }),
+    ...(req.query.category && { category: req.query.category }),
+    ...(req.query.slug && { slug: req.query.slug }),
+    ...(req.query.postId && { _id: req.query.postId }),
+    ...(req.query.searchTerm && {
+      $or: [
+        { title: { $regex: req.query.searchTerm, $options: 'i' } },
+        { content: { $regex: req.query.searchTerm, $options: 'i' } }
+      ]
+    })
+  })
+    .sort({ updatedAt: sortDirection })
+    .skip(startIndex)
+    .limit(limit)
+
+  const count = await Post.countDocuments()
+
+  const now = new Date()
+  const oneMonthAgo = new Date(
+    now.getFullYear(),
+    now.getMonth() - 1,
+    now.getDate()
+  )
+
+  const lastMonthPosts = await Post.countDocuments({
+    createdAt: { $gte: oneMonthAgo }
+  })
+
+  res.status(statusCode.OK).json({
+    message: 'Get posts successfully',
+    posts,
+    totalPosts: count,
+    lastMonthPosts
+  })
+})
--- backend/src/apis/routes/v1/post.routes.js
+++ backend/src/apis/routes/v1/post.routes.js
@@ -2,8 +2,9 @@ import express from 'express'
 const router = express.Router()
 
 import { authenticate } from '../../../middlewares/auth.middleware.js'
-import { create } from '../../controllers/post.controller.js'
+import { create, getPosts } from '../../controllers/post.controller.js'
 
 router.post('/create', authenticate, create)
+router.get('/getPosts', getPosts)
 
 export default router
