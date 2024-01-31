3318990d5ee1a890122dc99c289e16fb805856a8
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-01-31 21:40:58 +0700

    complete recent posts

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -3,12 +3,14 @@ import { Link, useParams } from 'react-router-dom'
 import { Button, Spinner } from 'flowbite-react'
 import CallToAction from '../components/CallToAction'
 import CommentSection from '../components/CommentSection'
+import PostCard from '../components/PostCard'
 
 function PostPage() {
   const { postSlug } = useParams()
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(false)
   const [post, setPost] = useState(null)
+  const [recentPosts, setRecentPosts] = useState(null)
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -37,6 +39,24 @@ function PostPage() {
     fetchPost()
   }, [postSlug])
 
+  useEffect(() => {
+    try {
+      const fetchRecentPosts = async () => {
+        const res = await fetch('http://localhost:8000/api/v1/posts/getPosts?limit=3')
+
+        const data = await res.json()
+
+        if (res.ok) {
+          setRecentPosts(data.posts)
+        }
+      }
+
+      fetchRecentPosts()
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
+
   if (loading) return <div className='flex justify-center items-center min-h-screen'>
     <Spinner size='xl' />
   </div>
@@ -62,6 +82,17 @@ function PostPage() {
         <CallToAction />
       </div>
       <CommentSection postId={post._id} />
+      <div className='flex flex-col justify-center items-center mb-5'>
+        <h1 className='text-xl mt-5'>Recent articles</h1>
+        <div className='flex flex-wrap gap-5 mt-5 justify-center'>
+          {
+            recentPosts &&
+            recentPosts.map(post => {
+              return <PostCard key={post._id} post={post} />
+            })
+          }
+        </div>
+      </div>
     </main>
   )
 }
