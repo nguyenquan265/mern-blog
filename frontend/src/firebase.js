d6fe4ff59df9b7686b956292101f3c5cee7bd8ca
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-02-17 10:43:53 +0700

    update views

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app'
+import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -6,7 +7,10 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  databaseURL:
+    'https://mern-blog-25a7f-default-rtdb.asia-southeast1.firebasedatabase.app'
 }
 
 export const app = initializeApp(firebaseConfig)
+export const database = getDatabase(app)
--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -4,6 +4,11 @@ import { Button, Spinner } from 'flowbite-react'
 import CallToAction from '../components/CallToAction'
 import CommentSection from '../components/CommentSection'
 import PostCard from '../components/PostCard'
+import { ref, get, runTransaction } from "firebase/database"
+import { database } from '../firebase'
+
+import { FaBookmark } from "react-icons/fa"
+import { GrView } from "react-icons/gr"
 
 function PostPage() {
   const { postSlug } = useParams()
@@ -11,6 +16,33 @@ function PostPage() {
   const [error, setError] = useState(false)
   const [post, setPost] = useState(null)
   const [recentPosts, setRecentPosts] = useState(null)
+  const [views, setViews] = useState(null)
+
+  const handleBookmark = () => { }
+
+  useEffect(() => {
+    if (post) {
+      const postRef = ref(database, 'posts/' + post._id + '/views');
+
+      runTransaction(postRef, (currentViews) => {
+        if (currentViews === null) {
+          return 1;
+        }
+
+        return currentViews + 1;
+      });
+
+      get(ref(database, `posts/${post._id}`)).then((snapshot) => {
+        if (snapshot.exists()) {
+          setViews((snapshot.val().views))
+        } else {
+          console.log("No data available")
+        }
+      }).catch((error) => {
+        console.error(error)
+      })
+    }
+  }, [post])
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -78,7 +110,18 @@ function PostPage() {
           {post.category}
         </Button>
       </Link>
-      <img src={post.image} alt={post.title} className='mt-10 p-3 max-h-[600px] w-full object-cover' />
+      <div class='flex justify-end p-3'>
+        <div class='flex items-center'>
+          <GrView class='h-5 w-5 mr-1 text-gray-500' />
+          <span class='text-gray-500'>{views}</span>
+        </div>
+        <FaBookmark
+          class='h-5 w-5 ml-4 hover:cursor-pointer hover:text-gray-500'
+          title='Bookmark this blog'
+          onClick={handleBookmark}
+        />
+      </div>
+      <img src={post.image} alt={post.title} className='mt-3 p-3 max-h-[600px] w-full object-cover' />
       <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
         <span>{new Date(post.createdAt).toLocaleDateString()}</span>
         <span className='italic'>{(post.content.length / 1000).toFixed(0)} mins read</span>
