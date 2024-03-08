79bce4d3253afab12a6522d28ebec7c2ecf4c740
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-03-08 22:18:30 +0700

    update

--- backend/src/utils/constants.js
+++ backend/src/utils/constants.js
@@ -1,4 +1,5 @@
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
-  'https://dancing-stroopwafel-56aa10.netlify.app'
+  'https://dancing-stroopwafel-56aa10.netlify.app',
+  'https://blog-web-pearl.vercel.app'
 ]
