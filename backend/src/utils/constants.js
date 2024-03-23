be7ca96a654867c4b766e78ef67b1fc9a381caa7
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-03-23 18:31:14 +0700

    udpate cron-job

--- backend/src/utils/constants.js
+++ backend/src/utils/constants.js
@@ -1,5 +1,7 @@
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
   'https://dancing-stroopwafel-56aa10.netlify.app',
-  'https://blog-web-pearl.vercel.app'
+  'https://blog-web-pearl.vercel.app',
+  'https://cron-job.org',
+  'https://console.cron-job.org'
 ]
